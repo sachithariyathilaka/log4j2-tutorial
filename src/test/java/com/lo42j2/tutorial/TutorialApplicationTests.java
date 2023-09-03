90b3c46227f3dff11a08453669939eabb40b8975
sachith-hy <sachith.vidu@yahoo.com>
2023-09-03 17:00:46 +0530

    Project commit

---
+++ src/test/java/com/lo42j2/tutorial/TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lo42j2.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
